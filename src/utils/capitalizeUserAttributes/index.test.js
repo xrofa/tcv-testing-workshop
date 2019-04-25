19b6ed5e44f880e29c589e0633d1f8fccf901b28
Christian Sousa <christian.sousa@thecodeventure.com>
2019-04-25 11:34:14 +0100

    Added more test to capitalizeUserAttributes

--- src/utils/capitalizeUserAttributes/index.test.js
+++ src/utils/capitalizeUserAttributes/index.test.js
@@ -1,21 +1,36 @@
 import capitalizeUserAttributes from './index'
 
+const user = {
+  firstName: 'pedro',
+  lastName: 'lopes',
+  age: 26,
+  email: 'pedro.lopes@thecodeventure.com'
+}
+
+const anotherUser = {
+  firstName: 'christian',
+  lastName: 'sousa',
+  age: 28,
+  email: 'christian.sousa@theventurecode.com'
+}
 // Example of a test using a schema to assert
 test('should capitalize user attributes', () => {
-  const user = {
-    firstName: 'pedro',
-    lastName: 'lopes',
+  expect(capitalizeUserAttributes(user)).toEqual({
+    firstName: 'Pedro',
+    lastName: 'Lopes',
     age: 26,
-    email: 'pedro.lopes@thecodeventure.com'
-  }
+    email: 'Pedro.lopes@thecodeventure.com'
+  })
 
-  const anotherUser = {
-    firstName: 'christian',
-    lastName: 'sousa',
+  expect(capitalizeUserAttributes(anotherUser)).toEqual({
+    firstName: 'Christian',
+    lastName: 'Sousa',
     age: 28,
-    email: 'christian.sousa@theventurecode.com'
-  }
+    email: 'Christian.sousa@theventurecode.com'
+  })
+})
 
+test('should match a well defined schema', () => {
   const schema = {
     firstName: expect.any(String),
     lastName: expect.any(String),
@@ -25,11 +40,4 @@ test('should capitalize user attributes', () => {
 
   expect(capitalizeUserAttributes(user)).toEqual(schema)
   expect(capitalizeUserAttributes(anotherUser)).not.toEqual(schema)
-
-  expect(capitalizeUserAttributes(user)).toEqual({
-    firstName: 'Pedro',
-    lastName: 'Lopes',
-    age: 26,
-    email: 'Pedro.lopes@thecodeventure.com'
-  })
 })
